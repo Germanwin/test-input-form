84148aa0cbf96660eba6c7bdb925a97fb23bd753
Germanwin <timelapser7766005@gmail.com>
2024-07-10 10:59:18 +0300

    finaly installed all needed features

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-link to="/penis">Penis</router-link>
+  <router-link class="bg-black" to="/penis">Penis</router-link>
   <router-link to="/">home</router-link>
+
+
   <router-view/>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
-<template>
-  <div class="home">
-    
-    hey 
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-
-
 export default {
   name: 'HomeView',
   components: {
-    
+  },
+  data () {
+    return {
+      names: ['pipi', 'pupu', 'john', 'alex', 'steve', 'mike', 'jose', 'momo', 'mama', 'popo', 'zebra', 'okey', 'radio']
+    }
   }
 }
 </script>
+
+<template>
+  <div class="home">
+    
+      <p v-for="name in names">{{ name }}</p>
+
+  </div>
+</template>
+
+
